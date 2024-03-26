92ab9102535f591cbb57f85a327103df9ef810a0
BagreyA <158076701+BagreyA@users.noreply.github.com>
2024-03-27 00:11:00 +0700

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+struct paral{
+
+    double a;
+    double b;
+    double h;
+};
+
+struct paral ran();
+
+void ver(struct paral y[], int n);
